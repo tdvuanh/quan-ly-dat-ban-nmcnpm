51064257bc09ad02142a75535252c337448ba33f
tduyvuanh <75930779+tdvuanh@users.noreply.github.com>
2025-11-07 11:35:25 +0700

    config unit test with vitest

--- booking-service-app-fe/vite.config.ts
+++ booking-service-app-fe/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, type UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
     tailwindcss(),
   ],
-});
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+  },
+} as UserConfig);
