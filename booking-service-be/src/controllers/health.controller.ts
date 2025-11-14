ea7538278e7053c87e47b0b4de2c9e8f4f7af614
tduyvuanh <75930779+tdvuanh@users.noreply.github.com>
2025-11-14 15:12:46 +0700

    remove test file

--- booking-service-be/src/controllers/health.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const getHealth = (_req: Request, res: Response) => {
-  res.json({ ok: true, service: "be-api", time: new Date().toISOString() });
-};
