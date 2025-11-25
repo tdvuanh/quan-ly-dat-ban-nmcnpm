8d38521ae08e9ad669bc9949073696efae7de8b7
LamSon69 <144024073+LamSon69@users.noreply.github.com>
2025-11-26 02:20:18 +0700

    fix 1

--- booking-service-app-fe/vite.config.ts
+++ booking-service-app-fe/vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig, type UserConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+// import { defineConfig, type UserConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   test: {
+//     environment: 'jsdom',
+//     globals: true,
+//     setupFiles: './src/setupTests.js',
+//   },
+// } as UserConfig);
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.js',
-  },
-} as UserConfig);
+  plugins: [react(), tailwindcss()],
+});
